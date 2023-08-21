b0f61c8f6152b09f6687014a6f76fa26747a4089
Mark Morley <mmorley0395@gmail.com>
2023-08-21 09:59:35 -0400

    prettier formatting

--- src/components/Map/MapboxMap.js
+++ src/components/Map/MapboxMap.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { setupClick, clickClear } from './interaction.js';
+import React, { useEffect, useState, useRef } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { setupClick, clickClear } from "./interaction.js";
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
 
 function MapboxMap({ connectionType }) {
   const mapContainer = useRef(null);
@@ -11,55 +12,52 @@ function MapboxMap({ connectionType }) {
   useEffect(() => {
     const mapInstance = new mapboxgl.Map({
       container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/dark-v11',
+      style: "mapbox://styles/mapbox/dark-v11",
       center: [-75.16, 39.95],
-      zoom: 9
+      zoom: 9,
     });
 
     const setupLayers = () => {
-
-      if (connectionType === 'bike') {
-
-        mapInstance.addLayer({
-          id: "lts",
-          type: "line",
-          source: "lts_tile",
-          "source-layer": "existing_conditions_lts",
-          paint: {
-            "line-width": 2,
-            "line-opacity": 1,
-            "line-color": {
-              property: "lts_score",
-              stops: [
-                [1, "green"],
-                [2, "light green"],
-                [3, "yellow"],
-                [4, "red"],
-              ],
+      if (connectionType === "bike") {
+        mapInstance.addLayer(
+          {
+            id: "lts",
+            type: "line",
+            source: "lts_tile",
+            "source-layer": "existing_conditions_lts",
+            paint: {
+              "line-width": 2,
+              "line-opacity": 1,
+              "line-color": {
+                property: "lts_score",
+                stops: [
+                  [1, "green"],
+                  [2, "light green"],
+                  [3, "yellow"],
+                  [4, "red"],
+                ],
+              },
             },
           },
-        },
           // add layer before road intersection layer
-          'road-label-simple'
+          "road-label-simple",
         );
-
-      } else if (connectionType === 'pedestrian') {
-
-        mapInstance.addLayer({
-          id: "sw",
-          type: "line",
-          source: "sw_tile",
-          "source-layer": "ped_lines",
-          paint: {
-            "line-width": 2,
-            "line-opacity": 1,
-            "line-color": "darkslategrey"
+      } else if (connectionType === "pedestrian") {
+        mapInstance.addLayer(
+          {
+            id: "sw",
+            type: "line",
+            source: "sw_tile",
+            "source-layer": "ped_lines",
+            paint: {
+              "line-width": 2,
+              "line-opacity": 1,
+              "line-color": "darkslategrey",
+            },
           },
-        },
-          'road-label-simple'
+          "road-label-simple",
         );
       }
-
 
       // mapInstance.addLayer({
       //   id: "clicked",
@@ -78,11 +76,9 @@ function MapboxMap({ connectionType }) {
       //   },
       // });
 
-
       // setupClick(mapInstance);
       // clickClear(mapInstance);
-
-    }
+    };
 
     mapInstance.on("load", () => {
       // LOAD DATA: add vector tileset from DVRPC's server
@@ -94,28 +90,21 @@ function MapboxMap({ connectionType }) {
         promoteId: "id",
       });
 
-
       mapInstance.addSource("sw_tile", {
         type: "vector",
         url: "https://www.tiles.dvrpc.org/data/pedestrian-network.json",
         minzoom: 8,
       });
-
     });
 
-
-    mapInstance.on('styledata', setupLayers);
+    mapInstance.on("styledata", setupLayers);
     setMap(mapInstance);
 
     // Clean up on component unmount
     return () => mapInstance.remove();
-
   }, [connectionType]);
-
 
   return <div ref={mapContainer} className="map-container" />;
 }
-
-
 
 export default MapboxMap;
