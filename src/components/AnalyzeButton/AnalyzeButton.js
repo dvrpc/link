1a57488f7c94864c8938164c664c25d6dab0bc91
Mark Morley <mmorley0395@gmail.com>
2023-09-19 16:05:02 -0400

    geojson being returned to console with unique id for each seg in proj

--- src/components/AnalyzeButton/AnalyzeButton.js
+++ src/components/AnalyzeButton/AnalyzeButton.js
@@ -1,26 +1,50 @@
 import React from "react";
-import { Button } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal, Button } from "@mantine/core";
 
 function AnalyzeButton({ draw }) {
-  const analyzeData = () => {
-    if (draw) {
-      const allFeatures = draw.getAll();
-      console.log(allFeatures);
-    }
+  const [project, setProject] = React.useState("");
+
+  const analyzeData = (allFeatures) => {
+    console.log(allFeatures);
   };
 
+  const applyProjectName = () => {
+    if (draw) {
+      const allFeatures = draw.getAll();
+      allFeatures.features.forEach((feature, index) => {
+        feature.properties.name = `${project}${index + 1}`;
+      });
+      analyzeData(allFeatures);
+    }
+    close();
+  };
+
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <Button
-      style={{
-        position: "absolute",
-        top: "90px",
-        left: "10px",
-        zIndex: 10,
-      }}
-      onClick={analyzeData}
-    >
-      Analyze
-    </Button>
+    <>
+      <Modal opened={opened} onClose={close} title="Name Segments">
+        <input
+          type="text"
+          value={project}
+          onChange={(e) => setProject(e.target.value)}
+          placeholder="Enter project name"
+        />
+        <Button onClick={applyProjectName}>Submit</Button>
+      </Modal>
+      <Button
+        style={{
+          position: "absolute",
+          top: "90px",
+          left: "10px",
+          zIndex: 10,
+        }}
+        onClick={open}
+      >
+        Analyze
+      </Button>
+    </>
   );
 }
 
