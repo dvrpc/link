d112a2b196da60ecf162d1927eb581d20b08162c
Mark Morley <mmorley0395@gmail.com>
2023-09-25 16:59:26 -0400

    login logout loosely set up

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,19 @@
-import React, { useState } from "react";
-import mapboxgl from "mapbox-gl";
-import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import MapboxMap from "../Map/MapboxMap";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
 import { themeConfig } from "./mantineTheme";
-import { HeaderSimple } from "../Header/Header";
-import AnalyzeButton from "../AnalyzeButton/AnalyzeButton";
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
+import MainComponent from "../MainComponent/MainComponent";
+import Login from "../Authentication/Login";
 
 export default function App() {
-  const [draw, setDraw] = useState(null);
-  const [connectionType, setConnectionType] = useState("bike");
-
   return (
     <MantineProvider theme={themeConfig} withGlobalStyles>
-      <div className="parent">
-        <HeaderSimple
-          connectionType={connectionType}
-          setConnectionType={setConnectionType}
-        />
-        <AnalyzeButton draw={draw} />
-        <MapboxMap setDraw={setDraw} connectionType={connectionType} />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<MainComponent />} />
+        </Routes>
+      </Router>
     </MantineProvider>
   );
 }
---
+++ src/components/MainComponent/MainComponent.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import MapboxMap from "../Map/MapboxMap";
+import { HeaderSimple } from "../Header/Header";
+import AnalyzeButton from "../AnalyzeButton/AnalyzeButton";
+import Logout from "../Authentication/Logout";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZHZycGNvbWFkIiwiYSI6ImNrczZlNDBkZzFnOG0ydm50bXR0dTJ4cGYifQ.VaJDo9EtH2JyzKm3cC0ypA";
+
+export default function MainComponent() {
+  const [draw, setDraw] = useState(null);
+  const [connectionType, setConnectionType] = useState("bike");
+
+  return (
+    <div className="parent">
+      <HeaderSimple
+        connectionType={connectionType}
+        setConnectionType={setConnectionType}
+      />
+      <AnalyzeButton draw={draw} />
+      <Logout />
+      <MapboxMap setDraw={setDraw} connectionType={connectionType} />
+    </div>
+  );
+}
