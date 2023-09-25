c7b0d93d7d76a4a87c684b8365e41a07a75ddcb4
Mark Morley <mmorley0395@gmail.com>
2023-09-25 16:01:37 -0400

    add auth0 and update how root rendered to fix depreciation error in react 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./index.css";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./components/App/App.js";
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Auth0Provider
+    domain="dev-m3mawqu2lofskvrl.us.auth0.com"
+    clientId="9uewlCua7Acm6jLOWPXd9SQnyNvqJABt"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
+  </Auth0Provider>,
 );
