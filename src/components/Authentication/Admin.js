83205bb0bf6edf89e152d2532a647c9c388a5746
Mark Morley <mmorley0395@gmail.com>
2023-12-18 14:31:41 -0500

    admin portal up

--- src/components/Authentication/Admin.js
+++ src/components/Authentication/Admin.js
@@ -1,7 +1,105 @@
-import React from "react";
-import { Center, Text } from "@mantine/core";
+import React, { useState, useEffect } from "react";
+import { Center, Text, Table, Button } from "@mantine/core";
 
 export default function Admin() {
+  const [studies, setStudies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [currentSchema, setCurrentSchema] = useState("lts");
+
+  useEffect(() => {
+    const fetchStudies = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/get_user_studies/?schema=${currentSchema}`,
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setStudies(data.studies);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchStudies();
+  }, [currentSchema]);
+
+  const toggleSchema = () => {
+    setCurrentSchema(currentSchema === "lts" ? "sidewalk" : "lts");
+  };
+
+  const renderTable = () => {
+    return (
+      <Table striped>
+        <thead>
+          <tr>
+            <th>username</th>
+            <th>seg_name</th>
+            <th>has_isochrone</th>
+            <th>miles</th>
+            <th>total_pop</th>
+            <th>disabled</th>
+            <th>ethnic_minority</th>
+            <th>female</th>
+            <th>foreign_born</th>
+            <th>lep</th>
+            <th>low_income</th>
+            <th>older_adult</th>
+            <th>racial_minority</th>
+            <th>youth</th>
+            <th>circuit</th>
+            <th>total_jobs</th>
+            <th>bike_ped_crashes</th>
+            <th>essential_services</th>
+            <th>rail_stations</th>
+            <th>deleted</th>
+          </tr>
+        </thead>
+        <tbody>
+          {studies.map((studies) => (
+            <tr key={studies.seg_name}>
+              <td>{studies.username}</td>
+              <td>{studies.seg_name}</td>
+              <td>{studies.has_isochrone ? "True" : "False"}</td>
+              <td>{studies.miles}</td>
+              <td>{studies.total_pop}</td>
+              <td>{studies.disabled}</td>
+              <td>{studies.ethnic_minority}</td>
+              <td>{studies.female}</td>
+              <td>{studies.foreign_born}</td>
+              <td>{studies.lep}</td>
+              <td>{studies.low_income}</td>
+              <td>{studies.older_adult}</td>
+              <td>{studies.racial_minority}</td>
+              <td>{studies.youth}</td>
+              <td>{studies.circuit}</td>
+              <td>{studies.total_jobs}</td>
+              <td>
+                {studies.bike_ped_crashes
+                  .map(
+                    (crash) =>
+                      `Bike: ${crash["Total Bike Crashes"]}, Ped: ${crash["Total Pedestrian Crashes"]}`,
+                  )
+                  .join(", ")}
+              </td>
+              <td>
+                {studies.essential_services
+                  .map((service) => `${service.type}: ${service.count}`)
+                  .join(", ")}
+              </td>
+              <td>{studies.rail_stations}</td>
+              <td>{studies.deleted ? "True" : "False"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  };
+
   return (
     <Center
       style={{
@@ -10,12 +108,24 @@ export default function Admin() {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
+        backgroundColor: "rgb(47, 79, 79)",
       }}
-      bg="rgb(47, 79, 79)"
     >
-      <div>
-        <Text size="xl">Welcome to DVRPC Connect.</Text>
-        <Text>You are an admin.</Text>
+      <div style={{ width: "90%", maxWidth: "1200px", margin: "20px" }}>
+        <Text size="xl">Admin Portal</Text>
+        <Button onClick={toggleSchema}>Current schema: {currentSchema}</Button>
+
+        <div
+          style={{ maxHeight: "80vh", overflowY: "auto", marginTop: "20px" }}
+        >
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : error ? (
+            <Text>Error: {error}</Text>
+          ) : (
+            renderTable()
+          )}
+        </div>
       </div>
     </Center>
   );
