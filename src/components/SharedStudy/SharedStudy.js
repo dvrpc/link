255f2e7b47c68798dc3c58eba00c14a275aea12f
Mark Morley <mmorley0395@gmail.com>
2024-01-16 15:13:39 -0500

    add map layer for current schema

--- src/components/SharedStudy/SharedStudy.js
+++ src/components/SharedStudy/SharedStudy.js
@@ -42,6 +42,73 @@ const SharedStudy = ({
         center: [-75.16, 40.05],
         zoom: 8.5,
       });
+      map.on("load", () => {
+        map.addSource("lts_tile", {
+          type: "vector",
+          url: "https://www.tiles.dvrpc.org/data/lts.json",
+          minzoom: 8,
+          promoteId: "id",
+        });
+        map.addSource("sw_tile", {
+          type: "vector",
+          url: "https://www.tiles.dvrpc.org/data/pedestrian-network.json",
+          minzoom: 8,
+        });
+        console.log(propSchema);
+        if (propSchema === "lts") {
+          map.addLayer(
+            {
+              id: "lts",
+              type: "line",
+              source: "lts_tile",
+              "source-layer": "existing_conditions_lts",
+              paint: {
+                "line-width": 1,
+                "line-opacity": {
+                  property: "lts_score",
+                  stops: [
+                    [1, 1],
+                    [2, 1],
+                    [3, 0.5],
+                    [4, 0.5],
+                  ],
+                },
+                "line-color": {
+                  property: "lts_score",
+                  stops: [
+                    [1, "green"],
+                    [2, "light green"],
+                    [3, "yellow"],
+                    [4, "red"],
+                  ],
+                },
+              },
+            },
+            "road-label-simple",
+          );
+        } else if (propSchema === "sidewalk") {
+          map.addLayer(
+            {
+              id: "sw",
+              type: "line",
+              source: "sw_tile",
+              "source-layer": "ped_lines",
+              paint: {
+                "line-width": 1,
+                "line-opacity": 1,
+                "line-color": [
+                  "match",
+                  ["get", "feat_type"],
+                  "UNMARKED",
+                  "#FF0000",
+                  "#00A36C",
+                ],
+              },
+            },
+            "road-label-simple",
+          );
+        }
+      });
     }
     setLoading(false);
   }, []);
@@ -55,8 +122,8 @@ const SharedStudy = ({
   // if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
   return (
-    <Stack style={{ height: "100vh" }}>
-      <Box>
+    <Stack style={{ height: "100vh", bg: "rgb(47, 79, 79)" }}>
+      <Box bg="rgb(47, 79, 79)">
         {study && (
           <div>
             <h2>Study Name: {study.studies[0].seg_name}</h2>
@@ -83,131 +150,3 @@ const SharedStudy = ({
 };
 
 export default SharedStudy;
-
-// const SharedStudy = ({
-//   username: propUsername,
-//   schema: propSchema,
-//   studyId,
-// }) => {
-//   const { user } = useAuth0();
-//   const [study, setStudy] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-//   const mapContainer = useRef(null);
-
-//   useEffect(() => {
-//     console.log("Map container ref:", mapContainer.current);
-//     if (mapContainer.current) {
-//       console.log("initializeing map");
-//       const mapInstance = new mapboxgl.Map({
-//         container: mapContainer.current,
-//         style: "mapbox://styles/mapbox/dark-v11", // Use a basic style for testing
-//         center: [-75.16, 40.05],
-//         zoom: 8.5,
-//       });
-//     }
-//     setLoading(false);
-//   }, []);
-
-// useEffect(() => {
-//   const authUsername = user?.nickname || propUsername;
-//   let resolvedSchema = propSchema;
-//   // const fetchStudy = async () => {
-//   //   try {
-//   //     const response = await fetch(
-//   //       `http://localhost:8000/get_user_studies/?username=${authUsername}&schema=${resolvedSchema}&study_name=${studyId}`,
-//   //     );
-//   //     if (!response.ok) throw new Error("Study not found");
-//   //     const data = await response.json();
-//   //     console.log(data.studies[0].shared);
-//   //     if (!data.studies[0].shared) throw new Error("Study not shared");
-//   //     setStudy(data);
-//   //   } catch (err) {
-//   //     setError(err.message);
-//   //   } finally {
-//   // setLoading(false);
-//   // }
-//   // };
-//   //   if (mapContainer.current) {
-//   //     console.log(mapContainer.current);
-//   //     const mapInstance = new mapboxgl.Map({
-//   //       container: mapContainer.current,
-//   //       style: "mapbox://styles/mapbox/dark-v11",
-//   //       center: [-75.16, 40.05],
-//   //       zoom: 8.5,
-//   //     });
-
-//   //     mapInstance.on("load", () => {
-//   //       mapInstance.addSource("lts_tile", {
-//   //         type: "vector",
-//   //         url: "https://www.tiles.dvrpc.org/data/lts.json",
-//   //         minzoom: 8,
-//   //         promoteId: "id",
-//   //       });
-//   //     });
-
-//   //     mapInstance.addSource("sw_tile", {
-//   //       type: "vector",
-//   //       url: "https://www.tiles.dvrpc.org/data/pedestrian-network.json",
-//   //       minzoom: 8,
-//   //     });
-
-//   //     if (resolvedSchema === "bike") {
-//   //       resolvedSchema = "lts";
-//   //       mapInstance.addLayer(
-//   //         {
-//   //           id: "lts",
-//   //           type: "line",
-//   //           source: "lts_tile",
-//   //           "source-layer": "existing_conditions_lts",
-//   //           paint: {
-//   //             "line-width": 1,
-//   //             "line-opacity": {
-//   //               property: "lts_score",
-//   //               stops: [
-//   //                 [1, 1],
-//   //                 [2, 1],
-//   //                 [3, 0.5],
-//   //                 [4, 0.5],
-//   //               ],
-//   //             },
-//   //             "line-color": {
-//   //               property: "lts_score",
-//   //               stops: [
-//   //                 [1, "green"],
-//   //                 [2, "light green"],
-//   //                 [3, "yellow"],
-//   //                 [4, "red"],
-//   //               ],
-//   //             },
-//   //           },
-//   //         },
-//   //         "road-label-simple",
-//   //       );
-//   //     } else if (resolvedSchema === "pedestrian") {
-//   //       resolvedSchema = "sidewalk";
-//   //       mapInstance.addLayer(
-//   //         {
-//   //           id: "sw",
-//   //           type: "line",
-//   //           source: "sw_tile",
-//   //           "source-layer": "ped_lines",
-//   //           paint: {
-//   //             "line-width": 1,
-//   //             "line-opacity": 1,
-//   //             "line-color": [
-//   //               "match",
-//   //               ["get", "feat_type"],
-//   //               "UNMARKED",
-//   //               "#FF0000",
-//   //               "#00A36C",
-//   //             ],
-//   //           },
-//   //         },
-//   //         "road-label-simple",
-//   //       );
-//   //     }
-
-//   //     //     fetchStudy();
-//   //   }
-//   // }, [user, propUsername, propSchema, studyId]);
