2f2fbe030b3808aab10c6270db636daecf336e9c
Mark Morley <mmorley0395@gmail.com>
2024-05-09 15:32:42 -0400

    add locale so nums are commad

--- src/components/StudyShelf/columns.js
+++ src/components/StudyShelf/columns.js
@@ -8,17 +8,72 @@ export const useColumns = () => useMemo(() => [
     Cell: ({ cell }) =>
       cell.getValue() === true ? 'Yes' : 'No',
   },
-  { accessorKey: 'miles', header: 'Miles' },
-  { accessorKey: 'total_pop', header: 'Total Population' },
-  { accessorKey: 'disabled', header: 'Disabled Individuals' },
-  { accessorKey: 'ethnic_minority', header: 'Ethnic Minorities' },
-  { accessorKey: 'female', header: 'Female' },
-  { accessorKey: 'foreign_born', header: 'Foreign Born' },
-  { accessorKey: 'lep', header: 'Limited English Proficiency (LEP)' },
-  { accessorKey: 'low_income', header: 'Low Income' },
-  { accessorKey: 'older_adult', header: 'Older Adults' },
-  { accessorKey: 'racial_minority', header: 'Racial Minorities' },
-  { accessorKey: 'youth', header: 'Youth' },
+  {
+    accessorKey: 'miles', header: 'Miles',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'total_pop', header: 'Total Population',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'disabled', header: 'Disabled Individuals',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'ethnic_minority', header: 'Ethnic Minorities',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'female', header: 'Female',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'foreign_born', header: 'Foreign Born',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'lep', header: 'Limited English Proficiency (LEP)',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'low_income', header: 'Low Income',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'older_adult', header: 'Older Adults',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'racial_minority', header: 'Racial Minorities',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
+  {
+    accessorKey: 'youth', header: 'Youth',
+    Cell: ({ cell }) =>
+      cell.getValue().toLocaleString('en-US', {
+      }),
+  },
   {
     accessorKey: 'circuit',
     header: 'Circuit',
