02e98dbcaa6bd9780e986d073f7de8689dd8a316
Mark Morley <mmorley0395@gmail.com>
2024-07-03 13:24:21 -0400

    pretty names

--- src/components/Map/RegionalHeader.js
+++ src/components/Map/RegionalHeader.js
@@ -1,11 +1,31 @@
 import { Container, NativeSelect, Flex } from '@mantine/core';
 import Logo from '../Logo/Logo'
 
+const attributeDisplayNames = {
+  total_pop: 'Total Population',
+  disabled: 'Disabled',
+  ethnic_minority: 'Ethnic Minority',
+  female: 'Female',
+  foreign_born: 'Foreign Born',
+  lep: 'Limited English Proficiency',
+  low_income: 'Low Income',
+  older_adult: 'Older Adult',
+  racial_minority: 'Racial Minority',
+  youth: 'Youth',
+  total_jobs: 'Total Jobs'
+};
+
+
 export default function RegionalHeader({ counties, attributes, setCurrentCounty, setCurrentAttribute }) {
+  const attributeOptions = attributes.map(attr => ({
+    value: attr,
+    label: attributeDisplayNames[attr] || attr
+  }));
+
   return (
     <>
       <Container fluid h={80} bg="rgb(47, 79, 79)">
-        <Flex gap='xl' align="center" >
+        <Flex gap='xl' align="center">
           <Logo logoWidth={'150px'} />
           <NativeSelect
             label="Selected County"
@@ -17,12 +37,10 @@ export default function RegionalHeader({ counties, attributes, setCurrentCounty,
             label="Current Attribute"
             placeholder="Total Population"
             onChange={(event) => setCurrentAttribute(event.currentTarget.value)}
-            data={attributes}
+            data={attributeOptions}
           />
         </Flex>
       </Container>
     </>
   );
 }
-
-
