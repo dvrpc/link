17b4b671a0e11bf60338bcb4aaad725fc14dd3c4
Mark Morley <mmorley0395@gmail.com>
2024-11-12 15:04:50 -0500

    add new legends per mikes comments

--- src/components/Legends/Legend.js
+++ src/components/Legends/Legend.js
@@ -9,18 +9,21 @@ import sidewalkLegend from "../../assets/Artboard 2.png"
 function Legend({ connectionType }) {
 
   var legend = 'none'
+  var label = 'none'
   if (connectionType == 'bike') {
     legend = ltsLegend
+    label = "LTS represents Level of Traffic Stress for cyclists, with a '1' being the least stressful, and a '4' being the most stressful. Read more in the Getting Started Guide. Your study and the low-stress area are visible when you draw or view one of your studies."
   }
   else {
     legend = sidewalkLegend
+    label = "Sidewalks are shown in green, unmarked crosswalks are shown in red. Draw a study to see the low-stress area it would connect!Your study and the low-stress area are visible when you draw or view one of your studies."
   }
   return (
     <>
       <Tooltip
         multiline
         w={220}
-        label="LTS represents Level of Traffic Stress for cyclists, with a '1' being the least stressful, and a '4' being the most stressful. Read more in the Getting Started Guide.">
+        label={label}>
         <Image
           radius="md"
 
@@ -30,7 +33,7 @@ function Legend({ connectionType }) {
             bottom: 30,
             right: 10,
             zIndex: 100,
-            width: '200px',
+            width: '250px',
             height: 'auto',
           }}
         />
