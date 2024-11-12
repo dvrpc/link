c5bb93a6f2c72a9b6bb05a29188077a82694c46c
Mark Morley <mmorley0395@gmail.com>
2024-11-12 10:09:47 -0500

    add tooltip for legend to explain lts

--- src/components/Legends/Legend.js
+++ src/components/Legends/Legend.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Image } from '@mantine/core';
+import { Tooltip, Image } from '@mantine/core';
 
 import ltsLegend from "../../assets/Artboard 1.png"
 import sidewalkLegend from "../../assets/Artboard 2.png"
@@ -16,19 +16,26 @@ function Legend({ connectionType }) {
     legend = sidewalkLegend
   }
   return (
-    <Image
-      radius="md"
+    <>
+      <Tooltip
+        multiline
+        w={220}
+        label="LTS represents Level of Traffic Stress for cyclists, with a '1' being the least stressful, and a '4' being the most stressful. Read more in the Getting Started Guide.">
+        <Image
+          radius="md"
 
-      src={legend}
-      style={{
-        position: 'fixed',
-        bottom: 30,
-        right: 10,
-        zIndex: 100,
-        width: '200px',
-        height: 'auto',
-      }}
-    />
+          src={legend}
+          style={{
+            position: 'fixed',
+            bottom: 30,
+            right: 10,
+            zIndex: 100,
+            width: '200px',
+            height: 'auto',
+          }}
+        />
+      </Tooltip>
+    </>
   );
 }
 
